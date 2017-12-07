a17abc25625216abb85484a3d786a76abc458cb6
dongwei <2642782820@qq.com>
2017-12-07 17:12:12 +0800

    Initial Commit

---
+++ Airpet/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Airpet
+//
+//  Created by 董伟 on 2017/12/7.
+//  Copyright © 2017年 董伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Airpet/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Airpet
+//
+//  Created by 董伟 on 2017/12/7.
+//  Copyright © 2017年 董伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
